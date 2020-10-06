7688a6b5fb9dfbdc3e21a2d68e1877c6b695c957
Sean <sm17595@essex.ac.uk>
2020-10-06 02:01:17 +0100

    Ambumate gui

---
+++ AmbuMate/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AmbuMate
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var assembly = typeof(MainPage);
+
+            iconImage.Source = ImageSource.FromResource("AmbuMate.Assets.Images.ambumatelogo.png", assembly);
+        }
+
+        private void LogInBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
