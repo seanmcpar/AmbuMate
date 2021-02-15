36b0b0652ac274eefca70d115b71df3202af5503
seanmcpar <68127255+seanmcpar@users.noreply.github.com>
2021-02-15 19:03:30 +0000

    Patient + Vehicle Classes implemented

--- AmbuControl/Models/Patient.cs
+++ AmbuControl/Models/Patient.cs
@@ -7,5 +7,20 @@ namespace AmbuControl.Models
 {
     public class Patient
     {
+        public string Id { get; set; }
+        public int Reference { get; set; }
+        public string ShiftID { get; set; }
+        public string Status { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string PickUp { get; set; }
+        public string DropOff { get; set; }
+        public string SpecialRequirements { get; set; }
+        public string Infectious { get; set; }
+        public DateTime ArrivePickUp { get; set; }
+        public DateTime DepartPickUp { get; set; }
+        public DateTime ArriveDropOff { get; set; }
+        public DateTime DepartDropOff { get; set; }
+        public string Notes { get; set; }
     }
 }
--- AmbuControl/Models/Vehicle.cs
+++ AmbuControl/Models/Vehicle.cs
@@ -7,5 +7,21 @@ namespace AmbuControl.Models
 {
     public class Vehicle
     {
+        public string Id { get; set; }
+        public string Registration { get; set; }
+        public string ShiftID { get; set; }
+        public int StartMileage { get; set; }
+        public string Lights { get; set; }
+        public string Signals { get; set; }
+        public string Sirens { get; set; }
+        public string FireExtinguisher { get; set; }
+        public string WarningTriangle { get; set; }
+        public string FireBlanket { get; set; }
+        public string Clean { get; set; }
+        public string DeepClean { get; set; }
+        public int Fuel { get; set; }
+        public int EndMileage { get; set; }
+        public string Status { get; set; }
+        public string VehicleNotes { get; set; }
     }
 }
